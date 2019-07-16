a82cd4dfc29a96e7dcf2e4b26b4f943ab41a66b4
drice82 <xujiabin82@gmail.com>
2019-07-16 17:41:02 +0800

    logout

--- app/Http/Controllers/DbPanelController.php
+++ app/Http/Controllers/DbPanelController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class DbPanelController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
    $data['tasks'] = [
