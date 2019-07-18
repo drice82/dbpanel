450e1ab8953c95a6da0a9809f96df5319e3a3ce6
drice82 <xujiabin82@gmail.com>
2019-07-18 10:26:48 +0800

    auth

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -7,11 +7,15 @@ use App\Hotel;
 
 class HotelController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(Request $request)
     {
         $keywords_name = $request->input('search');
         $data = Hotel::select('Name', 'CtfId', 'Gender', 'Birthday', 'Address', 'Mobile', 'Version') -> where('Name', $keywords_name)->get();
-//	$user_name = $results->Name;
         return view('dbpanel.hotel', compact('data'));
     }
 }
